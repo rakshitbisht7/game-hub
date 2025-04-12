2edd1582898108790861c4af70b784a90dc6f340
rakshitbisht7 <rakshitbisht1234@gmail.com>
2025-04-12 10:58:13 +0530

    Build game Trailer

---
+++ src/components/GameTrailer.tsx
@@ -0,0 +1,20 @@
+import userTrailers from "../hooks/useTrailers";
+
+interface Props {
+  gameId: number;
+}
+const GameTrailer = ({ gameId }: Props) => {
+  const { data, error, isLoading } = userTrailers(gameId);
+
+  if (isLoading) return null;
+
+  if (error) throw error;
+
+  const first = data?.results[0];
+  return first ? (
+    <video src={first.data[480]} poster={first.preview} controls />
+  ) : null;
+};
+
+export default GameTrailer;
+ 
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,8 +1,9 @@
 import { Heading, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
-import ExpandableText from "../components/Expandabletext";
+import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttributes";
 import useGame from "../hooks/useGame";
+import GameTrailer from "../components/GameTrailer";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -17,8 +18,10 @@ const GameDetailPage = () => {
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
       <GameAttributes game={game} />
+      <GameTrailer gameId={game.id} />
     </>
   );
 };
 
 export default GameDetailPage;
+ 
